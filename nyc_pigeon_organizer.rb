9efb7143a4c46d949bca451c9da6ae7155f81233
Logan Hasson <logan.hasson@gmail.com>
2014-01-15 13:44:06 -0500

    Complete solution

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+# Write your code here!
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+
+  data.each do |trait, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        organized[pigeon] ||= {}
+        if trait == :gender || trait == :lives
+          organized[pigeon][trait] = option.to_s
+        else
+          organized[pigeon][trait] ||= []
+          organized[pigeon][trait] << option.to_s
+        end
+      end
+    end
+  end
+
+  organized
 end
