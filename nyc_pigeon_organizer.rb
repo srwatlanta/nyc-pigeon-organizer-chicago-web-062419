640863156040f46f953e8b9920eae81b54db04a9
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-06-23 16:31:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  
+  data.each do |category, attribute|
+    attribute.each do |value, names|
+      names.each do |name|
+        new_pigeon_hash[name] = {:color => [], :gender =>, :lives => []}
+      end
+    end
+  end
+  data[:color].each do |value, names|
+    names.each do |name|
+      if data[:color][value].include?(name)
+        new_pigeon_hash[name][:color] << value.to_s
+      end
+    end
+  end
+  data[:gender].each do |value, names|
+    names.each do |name|
+      if data[:gender][value].include?(name)
+        new_pigeon_hash[name][:gender] << value.to_s
+      end
+    end
+  end
+  data[:lives].each do |value, names|
+    names.each do |name|
+      if data[:lives][value]include?(name)
+        new_pigeon_hash[name][:lives] << value.to_s
+      end
+    end
+  end
 end
